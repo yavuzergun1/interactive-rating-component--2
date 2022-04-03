543402e7a263708c760c2fa3d955219c76e5b473
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-04-03 19:27:54 +0300

    saved

--- index-2.js
+++ index-2.js
@@ -3,10 +3,11 @@ const mainContainer= document.querySelector('.container-main');
 const ratingContainer= document.querySelector('.container-rating');
 const ratings= document.querySelector('.nums');
 const thankU= document.querySelector('.container-thankU-none')
+const warn= document.querySelector('.container-warn-none')
 // const thankUContainer= document.querySelector('container-thankU');
 
 ratings.addEventListener('click',ratingYaz);
-let rating = 0;
+let rating = undefined;
 
 function ratingYaz(e){
     
@@ -19,7 +20,7 @@ submitButton.addEventListener('click', calistir);
 
 function calistir(e){
     e.preventDefault();
-    
+    if (rating) {
     thankU.className='container-thankU-block';
     thankU.innerHTML= `<img src="images/illustration-thank-you.svg" alt="">
     <div class="selected"> 
@@ -32,5 +33,12 @@ function calistir(e){
       <p> We appreciate you taking the time to give a rating. If you ever need more support, 
       don’t hesitate to get in touch!
       </p>`;
+    }else{
+      thankU.className='container-warn-block';
+    thankU.innerHTML= ` <img src="images/circle-exclamation-solid.svg" alt="" <i class="fa-solid fa-circle-exclamation"></i>> 
+      
+      <h1>Please Select any Rating!</h1>`;
+
+    }
     
 }
